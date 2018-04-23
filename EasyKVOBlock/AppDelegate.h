97dbe5e20a5198152a7d1fa7b514e8d089b351bc
lemo <lemo@lemodeiMac.local>
2018-04-23 09:14:13 +0800

    Initial Commit

---
+++ EasyKVOBlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyKVOBlock
+//
+//  Created by lemo on 2018/4/23.
+//  Copyright © 2018年 孙亚锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyKVOBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyKVOBlock
+//
+//  Created by lemo on 2018/4/23.
+//  Copyright © 2018年 孙亚锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
